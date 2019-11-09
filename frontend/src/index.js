559f90a700d64cd187babf9e3ab6ec354bc0d717
dstewart <davidstewart@gmail.com>
2019-11-09 14:22:33 -0700

    setup rails

---
+++ frontend/src/index.js
@@ -0,0 +1,14 @@
+const BASE_URL = "http://localhost:3000"
+const EQUIPMENT_URL = `${BASE_URL}/equipment`
+const NEWEQ_URL = `${BASE_URL}/equipment/new`
+const SCHEDULE_URL = `${BASE_URL}/schedule`
+
+// document.addEventListener("DOMContentLoaded", () => {
+//     fetchSchedules()
+//    }, false);
+
+//    let fetchSchedules = () => {
+//     return fetch(SCHEDULE_URL)
+//     .then(resp => resp.json())
+//     .then(json => renderTrainer(json));
+// };
