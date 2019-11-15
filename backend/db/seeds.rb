3c83ff18ef46cd23ad30512bef537b78631220f2
dsstewa <davidstewart@gmail.com>
2019-11-14 20:11:43 -0700

    final touches, commented out seed data

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -9,13 +9,13 @@
 
 
 
-pj_a = Project.create(name: "Shop", location: "Evergreen, CO", duration: "000")
-pj_b = Project.create(name: "Sylvan Lake State Park", location: "Eagle, CO", duration: "180")
-pj_c = Project.create(name: "Central 70", location: "Denver, CO", duration: "360")
-pj_d = Project.create(name: "River Run", location: "Granby, CO", duration: "660")
-pj_e = Project.create(name: "Brighton BLVD", location: "Denver, CO", duration: "160")
-eq_a = Equipment.create(make: "John Deere", model: "450 Excavator", rent: 33.23, project_id: 1 )
-eq_b = Equipment.create(make: "John Deere", model: "350 Excavator", rent: 23.23, project_id: 1)
-eq_c = Equipment.create(make: "John Deere", model: "340J Haul Truck", rent: 32.23, project_id: 1)
-eq_d = Equipment.create(make: "CAT", model: "320J Haul Truck", rent: 82.23, project_id: 1)
-eq_e = Equipment.create(make: "CAT", model: "350J Trench Truck", rent: 84.23, project_id: 2)
+#pj_a = Project.create(name: "Shop", location: "Evergreen, CO", duration: "000")
+#pj_b = Project.create(name: "Sylvan Lake State Park", location: "Eagle, CO", duration: "180")
+#pj_c = Project.create(name: "Central 70", location: "Denver, CO", duration: "360")
+#pj_d = Project.create(name: "River Run", location: "Granby, CO", duration: "660")
+#pj_e = Project.create(name: "Brighton BLVD", location: "Denver, CO", duration: "160")
+#eq_a = Equipment.create(make: "John Deere", model: "450 Excavator", rent: 33.23, project_id: 1 )
+#eq_b = Equipment.create(make: "John Deere", model: "350 Excavator", rent: 23.23, project_id: 1)
+#eq_c = Equipment.create(make: "John Deere", model: "340J Haul Truck", rent: 32.23, project_id: 1)
+#eq_d = Equipment.create(make: "CAT", model: "320J Haul Truck", rent: 82.23, project_id: 1)
+#eq_e = Equipment.create(make: "CAT", model: "350J Trench Truck", rent: 84.23, project_id: 2)
